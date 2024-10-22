65ae78a607f52475768e69c2b23a4ccf3a45345e
iron-condor <38388619+iron-condor@users.noreply.github.com>
2024-10-22 00:09:17 -0400

    Change theme, add fontawesome icon

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,20 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { ThemeToggleService } from '../theme-toggle.service';
+import { Theme, ThemeToggleService } from '../theme-toggle.service';
+import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'nav-bar',
   standalone: true,
-  imports: [NgbModule, RouterLink, RouterLinkActive],
+  imports: [NgbModule, RouterLink, RouterLinkActive, FontAwesomeModule],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
   constructor(private themeToggleService: ThemeToggleService) {}
 
+  moonIcon = faMoon;
+  sunIcon = faSun;
+
   getCurrentTheme() {
-    return this.themeToggleService.currentTheme;
+    return this.themeToggleService.currentTheme == Theme.LIGHT ? this.sunIcon : this.moonIcon;
   }
 
   toggleTheme() {
