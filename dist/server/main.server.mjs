e801782bf874c6eb6c74d067fee060fdec934a3e
iron-condor <38388619+iron-condor@users.noreply.github.com>
2025-01-14 01:12:00 -0500

    Clean up icons

--- dist/server/main.server.mjs
+++ dist/server/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-4EED3RZ4.mjs";import"./chunk-63MIO7B7.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
+import{a}from"./chunk-EGZYNJXG.mjs";import"./chunk-63MIO7B7.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
