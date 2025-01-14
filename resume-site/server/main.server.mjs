a19eb60a28ef38356a7621e5023d43ae4399d1a9
iron-condor <38388619+iron-condor@users.noreply.github.com>
2025-01-13 23:13:42 -0500

    Re-commit dist, fix path

--- resume-site/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-NDBKAQBR.mjs";import"./chunk-63MIO7B7.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-4EED3RZ4.mjs";import"./chunk-63MIO7B7.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
